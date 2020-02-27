713831094cf0ade07ec26316236125cb3f5de0a3
Tyler <tstickler17@gmail.com>
2020-02-27 12:18:24 -0800

    Built out generic networking for the backend

--- scheduler/Networking.swift
+++ scheduler/Networking.swift
@@ -11,30 +11,103 @@ import Alamofire
 import SwiftyJSON
 
 class Networking {
-    let baseURL = "https://scheduler-29a0a.firebaseio.com/"
+    // Base URL of database
+    private let baseURL = "https://scheduler-29a0a.firebaseio.com/"
     
-    public func getData(fromPath path: String, _ onComplete: @escaping (Any?, AFError?)->()) {
-        getRequest(path: path) {
+    // Retrieve information from the database
+    func getRequest(fromPath path: String, _ onComplete: @escaping (Any?, AFError?)->()) {
+        AF.request("\(baseURL)\(path).json", method: .get).responseJSON {
             response in
             
             switch response.result {
-            case .success :
-                if let value = response.value {
-                    onComplete(JSON(value), nil)
-                }
-            case .failure:
-                onComplete(nil, response.error!)
+            case .success(let value) :
+                let data = JSON(value)
+                onComplete(data, nil)
+            case .failure(let error):
+                onComplete(nil, error)
             }
         }
     }
     
-    private func getRequest(path: String, _ onComplete: @escaping (AFDataResponse<Any>)->()) {
-        AF.request("\(baseURL)\(path).json").responseJSON {
+    // Replace with params to the database
+    func putRequest(path: String, params: AnyEncodable, _ onComplete: @escaping (Any?, AFError?)->()) {
+        AF.request("\(baseURL)\(path).json",
+            method: .put,
+            parameters: params,
+            encoder: JSONParameterEncoder.default).responseJSON {
+                response in
+
+                switch response.result {
+                case .success(let value) :
+                    let data = JSON(value)
+                    onComplete(data, nil)
+                case .failure(let error):
+                    onComplete(nil, error)
+                }
+        }
+    }
+    
+    // Add params to the database. Firebase gives a name when doing this.
+    func postRequest(path: String, params: AnyEncodable, _ onComplete: @escaping (Any?, AFError?)->()) {
+        AF.request("\(baseURL)\(path).json",
+            method: .post,
+            parameters: params,
+            encoder: JSONParameterEncoder.default).responseJSON {
+                response in
+                
+                switch response.result {
+                case .success(let value) :
+                    let data = JSON(value)
+                    onComplete(data, nil)
+                case .failure(let error):
+                    onComplete(nil, error)
+                }
+        }
+    }
+    
+    // Update params in database. Can add with own name. Name is the end of path.
+    func patchRequest(path: String, params: AnyEncodable, _ onComplete: @escaping (Any?, AFError?)->()) {
+        AF.request("\(baseURL)\(path).json",
+            method: .patch,
+            parameters: params,
+            encoder: JSONParameterEncoder.default).responseJSON {
+                response in
+                
+                switch response.result {
+                case .success(let value) :
+                    let data = JSON(value)
+                    onComplete(data, nil)
+                case .failure(let error):
+                    onComplete(nil, error)
+                }
+        }
+    }
+    
+    // Remove from database at path.
+    func deleteRequest(path: String, _ onComplete: @escaping (Any?, AFError?)->()) {
+        AF.request("\(baseURL)\(path).json", method: .delete).responseJSON {
             response in
             
-            onComplete(response)
+            switch response.result {
+            case .success(let value) :
+                let data = JSON(value)
+                onComplete(data, nil)
+            case .failure(let error):
+                onComplete(nil, error)
+            }
         }
     }
 }
 
 
+// Provides a generic way of encoding all types that will be sent to the database.
+struct AnyEncodable: Encodable {
+    private let encode: (Encoder) throws -> Void
+    public init<T: Encodable>(_ wrapped: T) {
+        encode = wrapped.encode
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        try encode(encoder)
+    }
+}
--- scheduler/ViewController.swift
+++ scheduler/ViewController.swift
@@ -7,30 +7,10 @@
 //
 
 import UIKit
-import Alamofire
-import SwiftyJSON
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let networker = Networking()
-        networker.getData(fromPath: "users") {
-            data, err in
-            
-            if let error = err {
-                print("Error occurred fetching at path users err: \(error)")
-                return
-            }
-            
-            guard let data = data else { return }
-            
-            print(data)
-        }
     }
-
-
 }
-
-
